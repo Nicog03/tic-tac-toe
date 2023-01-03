8105947c146e0798286b25be0e30c09b588d5cff
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2023-01-03 15:00:36 -0300

    Reference error fixed

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function checkForWin() {
     //itterates horizontally through all squares
     for (let l=0;l<=6;l+=3) {
         for (let c=l;c<l+3;c++) {
-                if (Boolean(divs[l].player) && divs[c].player == divs[l].player) {
+                if (Boolean(squares[l].player) && squares[c].player == squares[l].player) {
 
                     if (c == l+2) {
                         alert('win')
@@ -35,7 +35,7 @@ function checkForWin() {
     //itterates vertically through all squares
     for (let c=0;c<=2;c++) {
         for (let l=c;l<=c+6;l+=3) {
-            if (Boolean(divs[c].player) && divs[l].player == divs[c].player) {
+            if (Boolean(squares[c].player) && squares[l].player == squares[c].player) {
 
                 if (l == c+6) {
                     alert('win')
@@ -46,14 +46,14 @@ function checkForWin() {
 
     //itterates diagonally to the right
     for (let i=0;i<=8;i+=4) {
-        if (Boolean(divs[0].player) && divs[0].player == divs[i].player) {
+        if (Boolean(squares[0].player) && squares[0].player == squares[i].player) {
             i == 8 ? alert('win') : null
         }
     }
 
     //itterates diagonally to the left
     for (let i=2;i<=6;i+=2) {
-        if (Boolean(divs[2].player) && divs[2].player == divs[i].player) {
+        if (Boolean(squares[2].player) && squares[2].player == squares[i].player) {
             i == 6 ? alert('win') : null
         }
     }
