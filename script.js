51e7a943108e4ba187af11642d69c260e824de0d
nicolas <nicolas.lima03@hotmail.com>
2023-03-12 11:24:43 -0300

    translate Players object module to a factory function

--- script.js
+++ script.js
@@ -1,22 +1,12 @@
 let squares = document.querySelectorAll('body > div > button')
 
-let playerX = (function() {
+const Player = (name, markSVG) =>  {
+    return {name, markSVG}
+}
 
-    let name = 'X'
+const Player1 = Player('X', '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z"></path></svg>')
 
-    let playerMarkSVG = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z"></path></svg>'
-
-    return {playerMarkSVG, name,}
-})()
-
-let playerO = (function() {
-
-    let name = 'O'
-
-    let playerMarkSVG = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12z"></path></svg>'
-
-    return {playerMarkSVG, name}
-})()
+const Player2 = Player( 'O', '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12z"></path></svg>')
 
 function checkForWin() {
     
